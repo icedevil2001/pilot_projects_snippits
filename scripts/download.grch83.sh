fbdd64b560658f4d83c65ccba5f6ec3270b65adb
Priyesh Rughani <priyesh.rughani@nanoporetech.com>
2023-03-01 13:08:07 -0800

    check if exists

--- scripts/download.grch83.sh
+++ scripts/download.grch83.sh
@@ -4,8 +4,12 @@
 ## https://lh3.github.io/2017/11/13/which-human-reference-genome-to-use 
 
 OUTPUTDIR=/data/reference/
-
+_URL="ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz"
 mkdir -p $OUTPUTDIR
 
-wget --directory-prefix $OUTPUTDIR ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+REF_GZ=$OUTPUTDIR/(basename $_URL)
 
+if [ ! -f $REF_GZ ]
+then
+    wget --directory-prefix $OUTPUTDIR $_URL
+if
